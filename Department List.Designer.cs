a5c563060873a599ddc1b4bc48e275db3ee90bdd
Shihan Mohamed <shihanmails@gmail.com>
2023-05-24 08:29:48 +0530

    FormDepatment

---
+++ Department List.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace EmployeeTracking
+{
+    partial class Department_List
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            dataGridView1 = new DataGridView();
+            panel1 = new Panel();
+            btnAdd = new Button();
+            btnEdit = new Button();
+            btnDelete = new Button();
+            btnClose = new Button();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Dock = DockStyle.Top;
+            dataGridView1.Location = new Point(0, 0);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowTemplate.Height = 25;
+            dataGridView1.Size = new Size(800, 359);
+            dataGridView1.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(btnClose);
+            panel1.Controls.Add(btnDelete);
+            panel1.Controls.Add(btnEdit);
+            panel1.Controls.Add(btnAdd);
+            panel1.Dock = DockStyle.Bottom;
+            panel1.Location = new Point(0, 350);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(800, 100);
+            panel1.TabIndex = 1;
+            // 
+            // btnAdd
+            // 
+            btnAdd.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            btnAdd.Location = new Point(26, 15);
+            btnAdd.Name = "btnAdd";
+            btnAdd.Size = new Size(176, 64);
+            btnAdd.TabIndex = 0;
+            btnAdd.Text = "Add";
+            btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // btnEdit
+            // 
+            btnEdit.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            btnEdit.Location = new Point(219, 15);
+            btnEdit.Name = "btnEdit";
+            btnEdit.Size = new Size(176, 64);
+            btnEdit.TabIndex = 1;
+            btnEdit.Text = "Edit";
+            btnEdit.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            btnDelete.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            btnDelete.Location = new Point(415, 15);
+            btnDelete.Name = "btnDelete";
+            btnDelete.Size = new Size(176, 64);
+            btnDelete.TabIndex = 2;
+            btnDelete.Text = "Delete";
+            btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // btnClose
+            // 
+            btnClose.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            btnClose.Location = new Point(608, 15);
+            btnClose.Name = "btnClose";
+            btnClose.Size = new Size(176, 64);
+            btnClose.TabIndex = 3;
+            btnClose.Text = "Close";
+            btnClose.UseVisualStyleBackColor = true;
+            btnClose.Click += btnClose_Click;
+            // 
+            // Department_List
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(panel1);
+            Controls.Add(dataGridView1);
+            Name = "Department_List";
+            Text = "Department_List";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            panel1.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private DataGridView dataGridView1;
+        private Panel panel1;
+        private Button btnEdit;
+        private Button btnAdd;
+        private Button btnClose;
+        private Button btnDelete;
+    }
+}
---
+++ Department.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace EmployeeTracking
+{
+    partial class Department
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            txtDepartment = new TextBox();
+            txtDeptAdd = new Button();
+            btnClose = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(21, 53);
+            label1.Name = "label1";
+            label1.Size = new Size(112, 25);
+            label1.TabIndex = 0;
+            label1.Text = "Department";
+            // 
+            // txtDepartment
+            // 
+            txtDepartment.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            txtDepartment.Location = new Point(157, 50);
+            txtDepartment.Name = "txtDepartment";
+            txtDepartment.Size = new Size(238, 33);
+            txtDepartment.TabIndex = 0;
+            // 
+            // txtDeptAdd
+            // 
+            txtDeptAdd.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            txtDeptAdd.Location = new Point(58, 118);
+            txtDeptAdd.Name = "txtDeptAdd";
+            txtDeptAdd.Size = new Size(139, 63);
+            txtDeptAdd.TabIndex = 1;
+            txtDeptAdd.Text = "Add";
+            txtDeptAdd.UseVisualStyleBackColor = true;
+            // 
+            // btnClose
+            // 
+            btnClose.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            btnClose.Location = new Point(221, 118);
+            btnClose.Name = "btnClose";
+            btnClose.Size = new Size(139, 63);
+            btnClose.TabIndex = 2;
+            btnClose.Text = "Close";
+            btnClose.UseVisualStyleBackColor = true;
+            btnClose.Click += btnClose_Click;
+            // 
+            // Department
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(252, 159, 91);
+            ClientSize = new Size(422, 242);
+            Controls.Add(btnClose);
+            Controls.Add(txtDeptAdd);
+            Controls.Add(txtDepartment);
+            Controls.Add(label1);
+            Name = "Department";
+            Text = "Department";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox txtDepartment;
+        private Button txtDeptAdd;
+        private Button btnClose;
+    }
+}
